d88f582c92917068753dcebfde7d05d89aaaeeed
rs8799 <107487962+rs8799@users.noreply.github.com>
2023-03-30 11:23:30 -0400

    wokrs!

---
+++ March 30 2023.js
@@ -0,0 +1,8 @@
+function chromosomeCheck(sperm) {
+  if(sperm==="XY"){
+    return 'Congratulations! You\'re going to have a son.'
+  }else if (sperm==="YX"){
+    return 'Congratulations! You\'re going to have a son.'
+  }else if(sperm==="XX")
+    return 'Congratulations! You\'re going to have a daughter.'
+}
