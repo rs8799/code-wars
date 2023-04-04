0f1277a4046a0ed47233134f1ec044194298975c
rs8799 <107487962+rs8799@users.noreply.github.com>
2023-04-03 22:00:53 -0400

    works, but took a while to solve

--- april 03, 2023 problem 3.js
+++ april 03, 2023 problem 3.js
@@ -7,7 +7,7 @@ function insertDash(num) {
   num = num.toString().split('')
 
   for(let i=0;i<num.length;i++){
-    if(num[i]%2 !== 0 && num[i+1]%2 !== 0 ){
+    if(num[i]%2 > 0 && num[i+1]%2 > 0 ){
       num[i] = num[i] + '-'
     }
   }
