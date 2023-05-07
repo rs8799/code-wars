69a9792a090aa90663673e3a2ad514f4a1f2984f
rs8799 <107487962+rs8799@users.noreply.github.com>
2023-05-07 14:57:09 -0400

    works

--- May 07, 2023.js
+++ May 07, 2023.js
@@ -1,8 +1,8 @@
 // Sum Numbers
 function sum (numbers) {
-    "use strict";
-    return numbers.reduce((acc,prev) => acc + prev,0);
-     
+  "use strict";
+  return numbers.reduce((acc,prev) => acc + prev,0);
+   
 };
 
 sum([1, 5.2, 4, 0, -1])
