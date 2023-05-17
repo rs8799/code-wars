ecf01a5831ba6710a96d863d35f3c1da98c676bb
rs8799 <107487962+rs8799@users.noreply.github.com>
2023-05-17 12:27:28 -0400

    works

---
+++ May 17, 2023 interview question.js
@@ -0,0 +1,8 @@
+//Create a for loop that iterates up to 100 while outputting "fizz" at multiples of 3, "buzz" at multiples of 5 and "fizzbuzz" at multiples of 3 and 5
+
+for(let i=0;i<=100;i++){
+  console.log("fizz","fizz","fizz")
+  console.log("buzz","buzz","buzz","buzz","buzz")
+  console.log("fizzbuzz","fizzbuzz","fizzbuzz")
+  console.log("fizzbuzz","fizzbuzz","fizzbuzz","fizzbuzz","fizzbuzz")
+}
