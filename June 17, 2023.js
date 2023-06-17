cec2c03791f9a896eef6e229beb8c376e02e78ce
rs8799 <107487962+rs8799@users.noreply.github.com>
2023-06-17 07:11:42 -0400

    works

---
+++ June 17, 2023.js
@@ -0,0 +1,31 @@
+function getPlanetName(id){
+ 
+  switch(id){
+    case 1:
+      return 'Mercury'
+      break
+    case 2:
+      return 'Venus'
+      break
+    case 3:
+      return 'Earth'
+      break
+    case 4:
+      return 'Mars'
+      break
+    case 5:
+      return 'Jupiter'
+      break
+    case 6:
+      return 'Saturn'
+      break
+    case 7:
+      return 'Uranus'
+      break
+    case 8:
+      return 'Neptune'
+      break
+  }
+  
+  return name;
+}
