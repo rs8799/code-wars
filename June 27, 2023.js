e122074d49bd32dd89e4da431888a92da9bcd07b
rs8799 <107487962+rs8799@users.noreply.github.com>
2023-06-27 07:29:02 -0400

    works

---
+++ June 27, 2023.js
@@ -0,0 +1,5 @@
+function position(letter){
+  let newArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  let position = newArray.indexOf(letter)+1
+  return `Position of alphabet: ${position}`
+  }
