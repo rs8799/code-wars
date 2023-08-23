d13e5e714799b69b35692d60d425dd9edcd67001
rs8799 <107487962+rs8799@users.noreply.github.com>
2023-08-23 07:12:39 -0400

    works

---
+++ Aug 23, 2023.js
@@ -0,0 +1,6 @@
+function isLockNessMonster(s) {
+  if (s.toLowerCase().includes('tree fiddy')) return true;
+  if (s.toLowerCase().includes('three fiddy')) return true;
+  if (s.toLowerCase().includes('3.50')) return true;
+  return false
+}
