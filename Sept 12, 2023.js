6b34fcdce3e35665c4be43bcac6d201c517933ff
rs8799 <107487962+rs8799@users.noreply.github.com>
2023-09-12 21:34:17 -0400

    works

---
+++ Sept 12, 2023.js
@@ -0,0 +1,10 @@
+function howManyDalmatians(number){
+  
+  const dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"];
+  
+  if (number <= 10) return dogs[0] 
+  if (number <= 50) return dogs[1]
+  if (number === 101) return dogs[3] 
+  return dogs[2]
+  
+}
