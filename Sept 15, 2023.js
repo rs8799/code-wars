89aefc67c4c4930622e57de42bd0196022eb9281
rs8799 <107487962+rs8799@users.noreply.github.com>
2023-09-15 20:11:42 -0400

    works

---
+++ Sept 15, 2023.js
@@ -0,0 +1,21 @@
+function getLength(arr){
+  //return length of arr
+  return arr.length
+}
+function getFirst(arr){
+  //return the first element of arr
+  return arr[0]
+}
+function getLast(arr){
+  //return the last element of arr
+  return arr[arr.length-1]
+}
+function pushElement(arr){
+  var el=1;
+  let newArray = arr.push(el)
+  return arr
+}
+function popElement(arr){
+let secondArray =  arr.pop() 
+  return arr
+}
