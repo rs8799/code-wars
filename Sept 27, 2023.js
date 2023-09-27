3ad82b5188341dbb280d13f6aea5533c466572ad
rs8799 <107487962+rs8799@users.noreply.github.com>
2023-09-27 06:39:20 -0400

    works

---
+++ Sept 27, 2023.js
@@ -0,0 +1,43 @@
+function howManydays(month){
+  var days;
+  switch (month){
+      case 1:
+      return 31
+      break;
+      case 3:
+      return 31
+      break;
+      case 5:
+      return 31
+      break;
+      case 7:
+      return 31
+      break;
+      case 8:
+      return 31
+      break;
+      case 10:
+      return 31
+      break;
+      case 12:
+      return 31
+      break;
+      case 4:
+      return 30
+      break;
+      case 6:
+      return 30
+      break;
+      case 9:
+      return 30
+      break;
+      case 11:
+      return 30
+      break;
+      case 2:
+      return 28
+      break;
+  
+  }
+  return days;
+}
