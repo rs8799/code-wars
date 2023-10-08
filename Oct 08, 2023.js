5747a572117b9015c51b5914d17f4e22ce018a8e
rs8799 <107487962+rs8799@users.noreply.github.com>
2023-10-08 07:32:51 -0400

    works

---
+++ Oct 08, 2023.js
@@ -0,0 +1,13 @@
+function calculator(a,b,sign){
+  if(sign=="+"){
+    return a+b
+  }else if(sign=="-"){
+    return a-b
+  }else if(sign=="*"){
+    return a*b
+  }else if(sign=="/"){
+    return a/b
+  }else
+    return "unknown value"
+
+}
