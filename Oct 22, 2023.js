df092588545c6deb8b39dbfbaee187001c6188b5
rs8799 <107487962+rs8799@users.noreply.github.com>
2023-10-22 07:27:04 -0400

    works

---
+++ Oct 22, 2023.js
@@ -0,0 +1,3 @@
+var quote = function(fighter) {
+  return "george saint pierre" ==fighter.toLowerCase() ? "I am not impressed by your performance.":"I'd like to take this chance to apologize.. To absolutely NOBODY!"
+  };
