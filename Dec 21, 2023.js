d8bbed75d5d36e087ec14a021142e4c51fa031ae
rs8799 <107487962+rs8799@users.noreply.github.com>
2023-12-21 06:57:53 -0500

    packagejson

---
+++ Dec 21, 2023.js
@@ -0,0 +1,7 @@
+const configuration = {
+  "name": "your-pack-name",
+  version:"1.1.1",
+  "description": ""
+};
+// Hint: try creating with `npm init` command
+// Help! -> https://docs.npmjs.com/files/package.json
