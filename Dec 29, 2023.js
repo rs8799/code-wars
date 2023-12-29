49f86528d72c298aea0bc5bfb97e9a9cda08e3d0
rs8799 <107487962+rs8799@users.noreply.github.com>
2023-12-29 06:56:17 -0500

    orderOfOperations

---
+++ Dec 29, 2023.js
@@ -0,0 +1,8 @@
+function orderOperations () {
+  return 2 + 2 * 2 + 2 * 2 + 22
+}
+//console.log(orderOperations)//32
+//can't change the + *, or add or remove the #2
+//can add number or operator, 
+//BEDMAS
+//2+4+4+22=10
