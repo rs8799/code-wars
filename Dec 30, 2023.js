a02e703cf6cde47a09faad497318a1fe914e13bb
rs8799 <107487962+rs8799@users.noreply.github.com>
2023-12-30 07:23:18 -0500

    generateLink

---
+++ Dec 30, 2023.js
@@ -0,0 +1,8 @@
+function generateLink(user) {
+  return `http://www.codewars.com/users/${encodeURIComponent(user)}`
+  //console.log(user+%20)
+}
+
+//user == string
+//one username
+//http://www.codewars.com/users/insert username
