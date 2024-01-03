e877fe295495b37b632bda6ed43ac6313d55e110
rs8799 <107487962+rs8799@users.noreply.github.com>
2024-01-03 06:52:18 -0500

    Counter

---
+++ Jan 03, 2023.js
@@ -0,0 +1,16 @@
+class Counter {
+  constructor(){
+    this.value = 0;
+  }
+   increase(){
+    this.value ++
+  }
+   getValue(){
+    return this.value
+  }
+   reset(){
+    this.value = 0
+  }
+  
+  
+}
