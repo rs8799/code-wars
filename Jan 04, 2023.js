694ddf87fdec5cdb5f851f61a7ef574fc107f0a8
rs8799 <107487962+rs8799@users.noreply.github.com>
2024-01-04 06:57:27 -0500

    greaterThan

---
+++ Jan 04, 2023.js
@@ -0,0 +1,10 @@
+function greaterThanLessThan (a,b,c) {
+  return a<b<c?true:false
+  
+  }
+  
+  //a < b < c || c < b < a
+  //ck is all argument are less the the previous on
+  
+  //console.log(greaterThanLessThan(3,5,7))//true
+  //console.log(greaterThanLessThan(3,8,7))//false
